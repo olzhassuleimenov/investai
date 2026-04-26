1828e9c8a2c1885d95aecb92ef3128943650ed68
Tair <tair.sagatov@bk.ru>
2026-04-26 22:42:57 +0500

    Initial commit

---
+++ InvestAI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InvestAI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
